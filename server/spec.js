f3b19127cdcd704434ce6f4d727e16894e6cda91
benjavaisberg <benjaminvaisberg96@gmail.com>
2020-05-07 16:07:14 -0400

    part5 complete

--- server/spec.js
+++ server/spec.js
@@ -17,8 +17,72 @@ describe('[LIONS]', function(){
       .end(function(err, resp) {
         expect(resp.body).to.be.an('array');
         done();
+      });
+    });
+
+    it('should create a lion', function(done) {
+      request(app)
+        .post('/lions')
+        .send({
+          name: 'Benja',
+          age: 24,
+          pride: 'Gangstas',
+          gender: 'male'
+        })
+        .set('Accept', 'application/json')
+        .expect('Content-Type', /json/)
+        .expect(201)
+        .end(function(err, resp) {
+          expect(resp.body).to.be.an('object');
+          done();
+        });
+    });
+
+    it('should delete a lion', function(done) {
+      request(app)
+      .post('/lions')
+      .send({
+        name: 'test',
+        age: 24,
+        pride: 'test',
+        gender: 'test'
       })
-  });
+      .set('Accept', 'application/json')
+      .end(function(err, resp) {
+        var lion = resp.body;
+        request(app)
+        .delete('/lions/' + lion.id)
+        .end(function(err, resp) {
+          expect(resp.body).to.eql(lion);
+          done();
+        });
+      });
+    });
+
+    it('should update a lion', function(done) {
+      request(app)
+        .post('/lions')
+        .send({
+          name: 'test',
+          age: 24,
+          pride: 'test',
+          gender: 'test'
+        })
+        .set('Accept', 'application/json')
+        .end(function(err, resp) {
+          var lion = resp.body;
+          request(app)
+            .put('/lions/' + lion.id)
+            .send({
+              name: 'mufaser'
+            })
+            .end(function(err, resp) {
+              expect(resp.body.name).to.equal('mufaser');
+              done();
+            });
+        });
+    });
+
 });
 
 
